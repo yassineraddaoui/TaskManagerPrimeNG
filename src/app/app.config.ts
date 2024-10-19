1be5eeff2aaeab676bba41e57618bb65d2d76b0b
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-10-19 15:04:43 +0100

    setting up ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import {StoreModule} from '@ngrx/store'
+import {EffectsModule} from '@ngrx/effects'
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { RouterState, StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    importProvidersFrom(
+
+      StoreModule.forRoot({
+        router: routerReducer,
+      }),
+      StoreRouterConnectingModule.forRoot({
+        stateKey: 'router',
+      }),
+      StoreDevtoolsModule.instrument(),
+      EffectsModule.forRoot([])
+    ),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./tasks/tasks.component').then((m) => m.TasksComponent),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+];
